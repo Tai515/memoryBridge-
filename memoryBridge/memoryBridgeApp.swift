ad19013e68e25a4816ec83e897828b558ec79cbe
Taina Saenz <tainamsaenz@gmail.com>
2023-08-01 14:32:11 -0700

    Initial Commit

---
+++ memoryBridge/memoryBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryBridgeApp.swift
+//  memoryBridge
+//
+//  Created by Taina Saenz on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct memoryBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
