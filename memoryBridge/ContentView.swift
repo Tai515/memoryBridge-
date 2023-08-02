1efdc7694f3d14ceae33c451885a384480287eb0
Taina Saenz <tainamsaenz@gmail.com>
2023-08-02 14:17:59 -0700

    New Memory Bridge Codes by Taina!!

--- memoryBridge/ContentView.swift
+++ memoryBridge/ContentView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textTitle = ""
     var body: some View {
-        VStack {
-            Text("Hey!")
+        NavigationStack {
+            VStack {
+                Text("Hello There! How Has Your Day Been Today?")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                Spacer()
+                Button("😁") {
+                    textTitle = "Good"
+                }
+                Spacer()
+                Button("😐") {
+                    textTitle = "Okay"
+                }
+                Spacer()
+                Button("☹️") {
+                    textTitle = "Bad"
+                }
+                Text(textTitle)
+                    .font(.title2)
+                    .buttonStyle(.borderedProminent)
+                    .tint(.blue)
+            }
+            .padding()
         }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
+    
 }
---
+++ memoryBridge/SecondView.swift
@@ -0,0 +1,22 @@
+//
+//  SecondView.swift
+//  memoryBridge
+//
+//  Created by Taina Saenz on 8/2/23.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        NavigationStack {
+            }
+        }
+    }
+    
+    struct SecondView_Previews: PreviewProvider {
+        static var previews: some View {
+            SecondView()
+        }
+    }
+
